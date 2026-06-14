aa42fd67ab4c4e8a385da8ddb152613064fdcd9e
newlisting4 <newlisting4@gmail.com>
2026-06-14 15:50:37 +0600

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://newback-ten.vercel.app";
+export const API_URL = "https://private-delight-api.vercel.app";
 
-export const site = "privatedelight.pro";
+export const site = "true-escorts.vercel.app";
